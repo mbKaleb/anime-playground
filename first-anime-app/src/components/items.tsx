59bbc50930be330df6583170ae1e28cf18330355
Kaleb Franken <kaleb.franken7@gmail.com>
2023-05-23 20:56:24 -0600

    reformatting flying box

--- first-anime-app/src/components/items.tsx
+++ first-anime-app/src/components/items.tsx
@@ -1,15 +1,46 @@
-// export default function() box = () => {
-// 	return (
-// 			<>  </>
-// 		)
-// }
-
-
+import { useEffect } from "react";
+import anime from "animejs";
 
 const Box = () => {
+	useEffect(() => {
+		const path = anime.path('path');
+		anime({
+			targets: "#box",
+			translateX: path('x'),
+			translateY: path("y"),
+			duration: 3000,
+			loop: true,
+			easing: 'linear'
+		});
+		anime({
+			targets: 'path',
+			opacity: 0,
+			duration: 6000,
+			loop: true,
+			direction: 'alternate',
+			easing: 'easeInOutExpo'
+		});
+	}, [])
 	return (<>
-		<div className="w-32 h-32 outline p-2 m-2 bg-gray-400 flex justify-center" > a</div>
+		<div className="box absolute top-1 left-79 w-2 h-2 bg-gray-400" id="box"  ></div>
 	</>) 
 }
 
-export { Box };
+
+const Box2 = () => {
+	return (<>
+		<div className="box absolute top-1 left-79 w-2 h-2 bg-gray-400" id="box"  ></div>
+	</>) 
+}
+
+const FlyingBox = (svg:any) => {
+	
+	return (
+		<div className="absolute flex justify-center ">
+			<Box2 />
+			<>{svg}</>
+		</div>
+	)
+}
+
+export { Box, FlyingBox };
